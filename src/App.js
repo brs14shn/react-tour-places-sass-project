266ef7ca92e6b102e54ff9ac2d1fbd20dc7c1875
brs14shn <brsshn292929@gmail.com>
2022-06-25 12:44:56 +0300

    Card.jsx added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.scss';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <>
     <Navbar/>
     <Header/>
+    <Main/>
+
       
     </>
   );
